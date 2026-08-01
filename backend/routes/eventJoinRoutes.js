60b5a59baab452a0ffbf77f4ea7934302f242385
prashant kumar <prashantk.hrs29@gmail.com>
2026-08-01 16:44:01 +0530

    Update eventJoinRoutes.js

--- backend/routes/eventJoinRoutes.js
+++ backend/routes/eventJoinRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { createJoin } = require("../controllers/eventJoinController");
+const { createJoin } = require("../controllers/eventjoinController");
 
 router.post("/", createJoin);
 
